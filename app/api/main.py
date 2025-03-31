1cf4f978b8089346f2a0253e7a1b620abdaebffa
AnJungMin <dkswjdals4027@gmail.com>
2025-03-31 21:16:48 +0900

    fix2

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 from app.api.predict import router as predict_router
 
-app = FastAPI(
-    title="Scalp Disease Classifier API",
-    description="멀티태스크 기반 두피 질환 분류 API",
-    version="1.0.0"
-)
+app = FastAPI()
 
-app.include_router(predict_router, prefix="/api")
+app.include_router(predict_router)
--- app/api/predict.py
+++ app/api/predict.py
@@ -6,20 +6,21 @@ from app.model.inference import load_model, predict
 
 router = APIRouter()
 
+# 경로는 Render 기준 상대경로로 조정
 model = load_model("app/model_weight/MobileVit-XXS_model_SCALP_2025_03_31_15.pt")
-
 
 @router.post("/predict")
 async def predict_disease(file: UploadFile = File(...)):
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-    results = predict(model, image)
-
-    return {
-        "task_1_mise": results[0],
-        "task_2_pizi": results[1],
-        "task_3_mosa": results[2],
-        "task_4_mono": results[3],
-        "task_5_biddem": results[4],
-        "task_6_talmo": results[5],
+    predictions = predict(model, image)
+    
+    response = {
+        "task_1_mise": predictions[0],
+        "task_2_pizi": predictions[1],
+        "task_3_mosa": predictions[2],
+        "task_4_mono": predictions[3],
+        "task_5_biddem": predictions[4],
+        "task_6_talmo": predictions[5],
     }
+    return response
