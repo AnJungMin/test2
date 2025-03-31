1dbde53d28ed708bcf76d80d9357c2a89fefd3ea
AnJungMin <dkswjdals4027@gmail.com>
2025-03-31 22:26:22 +0900

    inference.py fix5

--- app/model/inference.py
+++ app/model/inference.py
@@ -1,14 +1,14 @@
 import torch
-from PIL import Image
 from app.model.model import MultiTaskMobileViT
 from app.core.transform import transform  # transform은 이미지 전처리
+from PIL import Image
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_model(weight_path: str):
-    model = MultiTaskMobileViT(use_pretrained_backbone=True)  # True로 설정해야 pretrained 모델 로드
-    state = torch.load(weight_path, map_location=DEVICE, weights_only=True)  # ✅ weights_only=True 설정
-    model.load_state_dict(state["model_params"])
+    model = MultiTaskMobileViT(use_pretrained_backbone=True)  # True로 설정하여 pretrained 모델 로드
+    state = torch.load(weight_path, map_location=DEVICE)  # weights_only 파라미터 제거
+    model.load_state_dict(state["model_params"])  # 모델 파라미터만 로드
     model.to(DEVICE)
     model.eval()
     return model
