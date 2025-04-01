593f23bcd58dfe80d3b1114d66d2461ed97ad0a1
AnJungMin <dkswjdals4027@gmail.com>
2025-04-01 14:53:13 +0900

    inference.py fix8

--- app/model/inference.py
+++ app/model/inference.py
@@ -2,16 +2,12 @@ import torch
 from app.model.model import MultiTaskMobileViT
 from app.core.transform import transform  # transform은 이미지 전처리
 from PIL import Image
-from collections import defaultdict  # defaultdict 추가
-
-# defaultdict를 안전하게 로드할 수 있도록 설정
-torch.serialization.add_safe_globals([defaultdict])
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_model(weight_path: str):
     model = MultiTaskMobileViT(use_pretrained_backbone=True)  # True로 설정하여 pretrained 모델 로드
-    state = torch.load(weight_path, map_location=DEVICE, weights_only=True)  # weights_only=True로 설정하여 상태만 로드
+    state = torch.load(weight_path, map_location=DEVICE, weights_only=True)  # weights_only=True로 설정하여 모델 파라미터만 로드
     model.load_state_dict(state["model_params"])  # 모델 파라미터만 로드
     model.to(DEVICE)
     model.eval()
